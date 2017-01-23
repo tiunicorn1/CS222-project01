b0d636d04346e21febe05850ef7ee761f15f643b
Ray Harris <ray.harris@mcckc.edu>
2017-01-22 20:44:28 -0600

    Added all files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int squareWidth = 0;
+        int rectangleWidth = 0;
+        int rectangleHeight = 0;
+        int squareArea = 0;
+        int squarePerimeter = 0;
+        int rectangleArea = 0;
+        int rectanglePerimeter = 0;
+        Scanner scannerIn = new Scanner(System.in);
+
+        //  your code follows ...
+    }
+}
