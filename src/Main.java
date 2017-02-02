60cae35e05aa42bad0f390b05c4bdd6e0d5e2d97
Tiara Dalton <S1419209@Student.MCCKC.Edu>
2017-02-01 19:29:42 -0600

    Added modifiled files

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Main
 {
+
+    public static int calculateSquarePerimeter(int squareWidth)
+    {
+        return  (squareWidth * 4);
+    }
+
+    public static int calculateSquareArea(int squareWidth)
+    {
+        return  (squareWidth * squareWidth);
+    }
+
+    public static int calculateRectanglePerimeter(int rectangleWidth, int rectangleHeight)
+    {
+        return  (2 * (rectangleWidth + rectangleHeight));
+    }
+
+    public static int calculateRectangleArea(int rectangleWidth, int rectangleHeight)
+    {
+        return  (rectangleWidth * rectangleHeight);
+    }
     public static void main(String[] args)
     {
         int squareWidth = 0;
@@ -13,6 +33,31 @@ public class Main
         int rectanglePerimeter = 0;
         Scanner scannerIn = new Scanner(System.in);
 
-        //  your code follows ...
+        //  get input
+
+        System.out.print("Enter the width of the square: ");
+        squareWidth = scannerIn.nextInt();
+
+        System.out.print("Enter the width of the rectangle: ");
+        rectangleWidth = scannerIn.nextInt();
+
+        System.out.print("Enter the height of the rectangle: ");
+        rectangleHeight = scannerIn.nextInt();
+
+        // do calculations
+
+        squarePerimeter = calculateSquarePerimeter(squareWidth);
+        squareArea = calculateSquareArea(squareWidth);
+        rectanglePerimeter = calculateRectanglePerimeter(rectangleWidth, rectangleHeight);
+        rectangleArea = calculateRectangleArea(rectangleWidth, rectangleHeight);
+
+
+        //display results
+
+        System.out.println("The perimeter of the square is: " + squarePerimeter);
+        System.out.println("The area of the square is: "+ squareArea);
+        System.out.println("The perimeter of the rectangle is: " + rectanglePerimeter);
+        System.out.println("The area of the rectangle is: " + rectangleArea);
+
     }
 }
